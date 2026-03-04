c4ff8d9523111ca8e2510f6ed41f3f73080ae086
The-Vintage-Gabriel <thevintagegabriel@gmail.com>
2026-03-04 19:24:17 +0100

    button besser gemacht

--- button.js
+++ button.js
@@ -1,15 +1,40 @@
 btn = document.querySelector("button")
+let mouseCount = 0;
 
-btn.style.position = "relative";    
+btn.style.position = "relative";
 btn.addEventListener("mouseenter", (event) => {
     console.log("mouse entered");
-    
+
+
     let currentPixelstop = parseInt(event.target.style.top.replace("px", "")) || 0;
     let currentPixelsleft = parseInt(event.target.style.left.replace("px", "")) || 0;
-    currentPixelstop += 20;
-    currentPixelsleft += 20;
+    if (mouseCount < 100) {
+        if (currentPixelstop > 200) {
+            currentPixelstop -= Math.random() * 20 + 20;
+        } else {
+            if (Math.random * 2 == 0) {
+                currentPixelstop -= Math.random() * 20 + 20;
+            } else {
+                currentPixelstop += Math.random() * 20 + 20;
+            }
+        }
+
+        if (currentPixelsleft > 200) {
+            currentPixelsleft -= Math.random() * 20 + 20;
+        } else {
+            if (Math.random * 2 == 1) {
+                currentPixelsleft -= Math.random() * 20 + 20;
+            } else {
+                currentPixelsleft += Math.random() * 20 + 20;
+            }
+        }
+
+        mouseCount++;
+    }else {
+        btn.textContent = "You've tryed"
+    }
     event.target.style.top = currentPixelstop + "px";
     event.target.style.left = currentPixelsleft + "px";
-    }
+}
 )
 
